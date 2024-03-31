fb385ac68639e7e3775b00694106a7c0500a0a04
MinhHieu6 <165547208+MinhHieu6@users.noreply.github.com>
2024-03-31 14:52:33 +0700

    Add files via upload

---
+++ buổi 6.py
@@ -0,0 +1,15 @@
+#bài 2
+user = []
+
+print('wellcome to vừng ơi mở ra')
+a = input("nhập tên đăng kí: ")
+b = int(input("mời nhập mật khẩu: "))
+c = input("nhập tên đăng nhập: ")
+d = int(input('mời nhập mật khẩu đăng nhập: '))
+if a == c and b == d:
+    print("hello")
+
+else:
+    a != c or b != d
+    print("bạn đã nhập sai tên hoặc mật khẩu")
+
